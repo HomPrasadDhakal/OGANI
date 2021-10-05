88ecb4873307d9731a8a5485f6409d922da48279
raju7011 <rajudhakal7011gmail.com>
2021-10-05 13:22:32 +0545

    models, manager & signal and recivers for user model and profile

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from accounts.manager import Usermanger
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class user(AbstractUser):
+    username = None
+    last_login = None
+    email = models.EmailField(verbose_name='email', unique=True)
+    date_joined = models.DateTimeField(verbose_name="Add joined date", auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    
+
+    objects = Usermanger()
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name","last_name"]
+
+class Profile(models.Model):
+    user  = models.OneToOneField(user, on_delete=models.CASCADE)
+    address = models.CharField(max_length=255)
+    dob = models.DateField(null=True, blank=True)
+    gender = models.CharField(max_length=8)
+    profile_pic = models.FileField(upload_to="profile_pics")
+
+@receiver(post_save, sender=user)
+def create_or_save_user_profile(sender, created, instance,**kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
--- ogani/settings.py
+++ ogani/settings.py
@@ -135,3 +135,6 @@ STATICFILES_DIRS = [
 
 MEDIA_URL ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+
+AUTH_USER_MODEL  = 'accounts.user'
