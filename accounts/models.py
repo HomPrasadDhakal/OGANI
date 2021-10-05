49e3ad3562e9e92c187206eb8e5ce3f087086472
raju7011 <rajudhakal7011gmail.com>
2021-10-05 14:40:51 +0545

    creating login and register models & changes in user model

--- accounts/models.py
+++ accounts/models.py
@@ -13,6 +13,8 @@ class user(AbstractUser):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
+    is_customer = models.BooleanField(default=False)
+
     
 
     objects = Usermanger()
